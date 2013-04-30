b6c3d24d0e3b3b8ba4757e40d0b832a04f448a7d
regissimao <regissimao@5b94e99f-b59b-d2c2-3f9e-c1e51e61b361>
2013-04-30 23:33:04 +0000

    Criando classes do caso de uso Manter Piloto.

---
+++ Implementacao/racingbet/src/br/com/racingbet/entidade/Piloto.java
@@ -0,0 +1,63 @@
+package br.com.racingbet.entidade;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Piloto implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_piloto")
+	private Long id;
+	
+	@Column(nullable=false, length=50)
+	private String nome;
+	
+	@Column(nullable=false, length=25)
+	private String nacionalidade;
+	
+	@Column(nullable=false, length=2)
+	private String numeroDoCarro;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+
+	public String getNumeroDoCarro() {
+		return numeroDoCarro;
+	}
+
+	public void setNumeroDoCarro(String numeroDoCarro) {
+		this.numeroDoCarro = numeroDoCarro;
+	}
+
+
+}
