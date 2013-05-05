13390ecb2bd2300310d835e3f7c4f26587d7c894
thiagu.vf@gmail.com <thiagu.vf@gmail.com@5b94e99f-b59b-d2c2-3f9e-c1e51e61b361>
2013-05-05 04:05:11 +0000

    Adicionada as classes referentes ao UC Manter Resultado Grande Prêmio.

---
+++ trunk/Implementacao/racingbet/src/br/com/racingbet/dao/ResultadoGrandePremioDAO.java
@@ -0,0 +1,9 @@
+package br.com.racingbet.dao;
+
+import br.com.racingbet.entidade.ResultadoGrandePremio;
+
+
+public class ResultadoGrandePremioDAO extends GenericoDAO<ResultadoGrandePremio> {
+
+	private static final long serialVersionUID = 1L;
+}
