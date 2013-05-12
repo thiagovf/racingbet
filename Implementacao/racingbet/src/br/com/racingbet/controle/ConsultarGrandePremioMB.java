f5f900951840f5474f35de26d670250acb268975
bbeiruth@gmail.com <bbeiruth@gmail.com@5b94e99f-b59b-d2c2-3f9e-c1e51e61b361>
2013-05-12 19:04:09 +0000

    Implementação do caso de uso Consultar Grande Prêmio. O Persistence não foi enviado para não comprometer o funcionamento dos demais casos de uso.

---
+++ Implementacao/racingbet/src/br/com/racingbet/controle/ConsultarGrandePremioMB.java
@@ -0,0 +1,68 @@
+package br.com.racingbet.controle;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.racingbet.entidade.GrandePremio;
+import br.com.racingbet.servico.GrandePremioServico;
+
+@SuppressWarnings("serial")
+@Named("consultarGrandePremioMB")
+@SessionScoped
+
+public class ConsultarGrandePremioMB implements Serializable{
+	
+	@Inject
+	private Conversation conversacao;
+	
+	private List<GrandePremio> grandesPremios;
+	
+	@Inject
+	private GrandePremioServico grandePremioServico;
+	
+	public Conversation getConversacao() {
+		return conversacao;
+	}
+
+	
+	public void setConversacao(Conversation conversacao){
+		this.conversacao = conversacao;
+			
+	}
+	
+	@PostConstruct
+	public void init(){
+		grandesPremios = grandePremioServico.recuperarTodos();
+	}
+	
+	public String inciar(){
+		return "consultarGrandePremio";
+	}
+
+
+	public List<GrandePremio> getGrandesPremios() {
+		return grandesPremios;
+	}
+
+
+	public void setGrandesPremios(List<GrandePremio> grandesPremios) {
+		this.grandesPremios = grandesPremios;
+	}
+
+
+	public GrandePremioServico getGrandePremioServico() {
+		return grandePremioServico;
+	}
+
+
+	public void setGrandePremioServico(GrandePremioServico grandePremioServico) {
+		this.grandePremioServico = grandePremioServico;
+	}
+	
+}
---
+++ Implementacao/racingbet/src/br/com/racingbet/dao/ConsultarGrandePremioDAO.java
@@ -0,0 +1,12 @@
+package br.com.racingbet.dao;
+
+import br.com.racingbet.entidade.ConsultarGrandePremio;
+
+public class ConsultarGrandePremioDAO extends GenericoDAO<ConsultarGrandePremio> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
