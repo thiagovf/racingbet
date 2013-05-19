80954737b170f0a83945768dceb5bfdeb4b8a2a3
pedrofuques <pedrofuques@5b94e99f-b59b-d2c2-3f9e-c1e51e61b361>
2013-05-19 16:08:13 +0000

    Criacao dos Artefatos para: Manter Usuario, Manter Pontuacao e Consultar Pontuaao (Ranking)

---
+++ Implementacao/racingbet/src/br/com/racingbet/dao/UsuarioDAO.java
@@ -0,0 +1,12 @@
+package br.com.racingbet.dao;
+
+import br.com.racingbet.entidade.Usuario;
+
+public class UsuarioDAO extends GenericoDAO<Usuario> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
