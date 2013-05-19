bb5452cfbcbaf222a3c7ea55d1ca474366b7469e
pedrofuques <pedrofuques@5b94e99f-b59b-d2c2-3f9e-c1e51e61b361>
2013-05-19 16:08:13 +0000

    Criacao dos Artefatos para: Manter Usuario, Manter Pontuacao e Consultar Pontuaao (Ranking)

---
+++ trunk/Implementacao/racingbet/src/br/com/racingbet/dao/PontuacaoDAO.java
@@ -0,0 +1,17 @@
+package br.com.racingbet.dao;
+
+import java.util.Date;
+
+import org.hibernate.transform.ToListResultTransformer;
+
+import br.com.racingbet.entidade.Pontuacao;
+import br.com.racingbet.entidade.Usuario;
+
+public class PontuacaoDAO extends GenericoDAO<Pontuacao> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
