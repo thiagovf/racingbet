01308a0a56221d95c81bbb6297af26ecf01a255f
thiagu.vf@gmail.com <thiagu.vf@gmail.com@5b94e99f-b59b-d2c2-3f9e-c1e51e61b361>
2013-06-06 20:41:25 +0000

    Ajustes para o dropbox em cascata.

--- Implementacao/racingbet/src/br/com/racingbet/controle/ManterResultadoGrandePremioMB.java
+++ Implementacao/racingbet/src/br/com/racingbet/controle/ManterResultadoGrandePremioMB.java
@@ -2,7 +2,9 @@ package br.com.racingbet.controle;
 
 import java.io.Serializable;
 import java.text.ParseException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.Conversation;
@@ -10,10 +12,13 @@ import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import br.com.racingbet.entidade.Categoria;
 import br.com.racingbet.entidade.ResultadoGrandePremio;
+import br.com.racingbet.servico.CategoriaServico;
 import br.com.racingbet.servico.ResultadoGrandePremioServico;
 import br.com.racingbet.util.ConversacaoUtil;
 
+@SuppressWarnings("serial")
 @Named("manterResultadoGrandePremioMB")
 @SessionScoped
 public class ManterResultadoGrandePremioMB implements Serializable {
@@ -24,6 +29,85 @@ public class ManterResultadoGrandePremioMB implements Serializable {
 	
 	@Inject
 	private ResultadoGrandePremio resultadoGrandePremio;
+	
+	@Inject
+	private CategoriaServico catServico;
+	
+	private static Map<String, String> categorias;
+	
+	public static Map<String, String> grandePremios;
+	
+	public static Map<String, String> pilotos;
+	
+	private String categoria = "";
+	
+	private String grandePremio = "";
+	
+	private String piloto1 = "";
+	
+	private String piloto2 = "";
+	
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	public String getGrandePremio() {
+		return grandePremio;
+	}
+	
+	public String getPiloto1() {
+		return piloto1;
+	}
+
+	public void setPiloto1(String piloto1) {
+		this.piloto1 = piloto1;
+	}
+
+	public String getPiloto2() {
+		return piloto2;
+	}
+
+	public void setPiloto2(String piloto2) {
+		this.piloto2 = piloto2;
+	}
+
+	public void setGrandePremio(String grandePremio) {
+		this.grandePremio = grandePremio;
+	}
+
+	public Map<String, String> getCategoriasInMap() {
+		Map<String, String> countries = new LinkedHashMap<String, String>();
+		List<Categoria> lsCategorias = catServico.recuperarTodos();
+		for (Categoria categoria : lsCategorias) {
+			countries.put(categoria.getDescricao(), categoria.getId().toString());
+		}
+		categorias = countries;
+		return categorias;
+	}
+	
+	public Map<String, String> getGrandePremiosInMap() {
+		if(grandePremios == null) {
+			grandePremios = new LinkedHashMap<String, String>();
+		}
+		return grandePremios;
+	}
+	
+	public Map<String, String> getPilotosInMap() {
+		if(pilotos == null) {
+			pilotos = new LinkedHashMap<String, String>();
+		}
+		return pilotos;
+	}
+	
+	
+//	@SuppressWarnings("static-access")
+//	public void setGrandePremios(Map<String, String> grandePremios) {
+//		this.grandePremios = grandePremios;
+//	}
 
 	private List<ResultadoGrandePremio> resultadosGrandesPremios;
 	
