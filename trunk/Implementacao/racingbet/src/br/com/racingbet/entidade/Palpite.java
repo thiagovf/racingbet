0718ba1be584d6df065be601a1dc8bcc84475600
marcosfilho88@gmail.com <marcosfilho88@gmail.com@5b94e99f-b59b-d2c2-3f9e-c1e51e61b361>
2013-06-11 23:37:42 +0000

    atualizizado

--- trunk/Implementacao/racingbet/src/br/com/racingbet/entidade/Palpite.java
+++ trunk/Implementacao/racingbet/src/br/com/racingbet/entidade/Palpite.java
@@ -10,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -23,19 +24,21 @@ public class Palpite implements Serializable {
 	@Column(name = "id_palpite")
 	private Long id;
 
-	@Column(nullable = false, length = 50)
-	private String respostaPrimeiroColocado;
-
-	@Column(nullable = false, length = 50)
-	private String respostaPole;
+    @OneToOne(optional=false)
+    @JoinColumn(name="idPilotoPerguntaPole", nullable=false, insertable=true, updatable=true)
+	private Piloto repostaPerguntaPole;
+	
+    @OneToOne(optional=false)
+    @JoinColumn(name="idPilotoPerguntaPrimeiro", nullable=false, insertable=true, updatable=true)
+	private Piloto repostaPerguntaPrimeiro;
+	
+	@OneToOne(optional=false)
+	@JoinColumn(name="idGrandePremio",  nullable=false, insertable=true, updatable=true)
+	private GrandePremio grandePremio;
 
 	@ManyToOne
 	@JoinColumn(name="id_usuario")
 	private Usuario usuario;
-	
-	@ManyToOne
-	@JoinColumn(name="id_grandepremio")
-	private GrandePremio grandePremio;
 	
 	@Column(nullable = false)
 	@Temporal(TemporalType.DATE)
@@ -49,21 +52,20 @@ public class Palpite implements Serializable {
 		this.id = id;
 	}
 
-
-	public String getRespostaPole() {
-		return respostaPole;
+	public Piloto getRepostaPerguntaPole() {
+		return repostaPerguntaPole;
 	}
 
-	public void setRespostaPole(String respostaPole) {
-		this.respostaPole = respostaPole;
+	public void setRepostaPerguntaPole(Piloto repostaPerguntaPole) {
+		this.repostaPerguntaPole = repostaPerguntaPole;
 	}
 
-	public Usuario getUsuario() {
-		return usuario;
+	public Piloto getRepostaPerguntaPrimeiro() {
+		return repostaPerguntaPrimeiro;
 	}
 
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
+	public void setRepostaPerguntaPrimeiro(Piloto repostaPerguntaPrimeiro) {
+		this.repostaPerguntaPrimeiro = repostaPerguntaPrimeiro;
 	}
 
 	public GrandePremio getGrandePremio() {
@@ -74,6 +76,14 @@ public class Palpite implements Serializable {
 		this.grandePremio = grandePremio;
 	}
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
 	public Date getData() {
 		return data;
 	}
@@ -81,13 +91,6 @@ public class Palpite implements Serializable {
 	public void setData(Date data) {
 		this.data = data;
 	}
-	
-	public String getRespostaPrimeiroColocado() {
-		return respostaPrimeiroColocado;
-	}
 
-	public void setRespostaPrimeiroColocado(String respostaPrimeiroColocado) {
-		this.respostaPrimeiroColocado = respostaPrimeiroColocado;
-	}
-	
+		
 }
