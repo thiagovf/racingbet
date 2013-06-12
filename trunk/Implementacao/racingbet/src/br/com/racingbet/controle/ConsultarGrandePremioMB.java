802e7198c14f4910566b822288ceb0b9a04d553a
bbeiruth@gmail.com <bbeiruth@gmail.com@5b94e99f-b59b-d2c2-3f9e-c1e51e61b361>
2013-06-12 01:38:07 +0000

    Alterações no consultarGP

--- trunk/Implementacao/racingbet/src/br/com/racingbet/controle/ConsultarGrandePremioMB.java
+++ trunk/Implementacao/racingbet/src/br/com/racingbet/controle/ConsultarGrandePremioMB.java
@@ -1,21 +1,91 @@
 package br.com.racingbet.controle;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.Conversation;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.model.SelectItem;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import br.com.racingbet.entidade.Categoria;
 import br.com.racingbet.entidade.GrandePremio;
+import br.com.racingbet.servico.CategoriaServico;
 import br.com.racingbet.servico.GrandePremioServico;
 
 @SuppressWarnings("serial")
 @Named("consultarGrandePremioMB")
 @SessionScoped
 public class ConsultarGrandePremioMB implements Serializable {
+	
+	/* -------- Atributos necessarios para o ListBox de Categoria -------*/
+    private List<SelectItem> listaCategorias;
+    private String codigoCategoriaSelecionada;
+    @Inject
+    private CategoriaServico categoriaServico;
+    private Categoria categoriaSelecionada;
+ /* -------- Fim dos Atributos necessarios para o ListBox de Categoria -------*/
+
+/* -------- Metodos necessarios para o ListBox de Categoria -------*/
+
+    public List<SelectItem> getListaCategorias() {
+       
+        List<Categoria> categorias = categoriaServico.recuperarTodos();
+        Categoria categ;
+       
+        listaCategorias = new ArrayList<SelectItem>();
+        for (int i=0; i< categorias.size(); i++) {
+            categ = (Categoria) categorias.get(i);
+            listaCategorias.add(new SelectItem(categ.getId(), categ.getNome()));
+        }
+       
+        if ((codigoCategoriaSelecionada == null) || (codigoCategoriaSelecionada.equals(""))) {
+            if ((categorias != null) && (categorias.size() > 0)) {
+                categoriaSelecionada = categorias.get(0);
+                codigoCategoriaSelecionada = categoriaSelecionada.getId().toString();
+            }
+        }
+       
+        return listaCategorias;
+    }
+   
+    public void recuperarCategoria() {
+        categoriaSelecionada = categoriaServico.recuperarPorId(Long.valueOf(codigoCategoriaSelecionada));
+        System.out.println("Categoria="+categoriaSelecionada.getNome());
+    }
+
+    public void setListaCategorias(List<SelectItem> listaCategorias) {
+        this.listaCategorias = listaCategorias;
+    }
+
+    public String getCodigoCategoriaSelecionada() {
+        return codigoCategoriaSelecionada;
+    }
+
+    public void setCodigoCategoriaSelecionada(String codigoCategoriaSelecionada) {
+        this.codigoCategoriaSelecionada = codigoCategoriaSelecionada;
+    }
+
+    public CategoriaServico getCategoriaServico() {
+        return categoriaServico;
+    }
+
+    public void setCategoriaServico(CategoriaServico categoriaServico) {
+        this.categoriaServico = categoriaServico;
+    }
+
+    public Categoria getCategoriaSelecionada() {
+        return categoriaSelecionada;
+    }
+
+    public void setCategoriaSelecionada(Categoria categoriaSelecionada) {
+        this.categoriaSelecionada = categoriaSelecionada;
+    }
+   
+    /* -------- Fim dos Metodos necessarios para o ListBox de Categoria -------*/
 
 	@Inject
 	private Conversation conversacao;
